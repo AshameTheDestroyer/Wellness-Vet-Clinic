166ab2adfa3f3e611ae0a0e732cd26afe7efbd1f
Hashem Wannous <hashemwnoos@gmail.com>
2023-12-07 16:24:45 +0200

    Deployment bug fixes.

--- src/utils/components/ThemeContext/ThemeContext.tsx
+++ src/utils/components/ThemeContext/ThemeContext.tsx
@@ -1,46 +1,46 @@
-import { useSelector } from "react-redux";
-import React, { useEffect, useState } from "react";
+// import { useSelector } from "react-redux";
+// import React, { useEffect, useState } from "react";
 
-import Theme from "../../types/Theme";
-import { ConvertSystemTheme, themeMedia } from "../../functions/GetCurrentTheme";
-import { SelectPreferenceSlice } from "../../../Store/Features/PreferenceSlice/PreferenceSlice";
+// import Theme from "../../types/Theme";
+// import { ConvertSystemTheme, themeMedia } from "../../functions/GetCurrentTheme";
+// import { SelectPreferenceSlice } from "../../../Store/Features/PreferenceSlice/PreferenceSlice";
 
-type ThemeContextProps = {
-    root: HTMLElement;
-    themes: Map<Theme, React.CSSProperties>;
-};
+// type ThemeContextProps = {
+//     root: HTMLElement;
+//     themes: Map<Theme, React.CSSProperties>;
+// };
 
-export default function ThemeContext(props: ThemeContextProps): React.ReactElement {
-    const PreferenceSlice = useSelector(SelectPreferenceSlice);
-    const [theme, setTheme] = useState(ConvertSystemTheme(PreferenceSlice.handlers.theme));
+// export default function ThemeContext(props: ThemeContextProps): React.ReactElement {
+//     const PreferenceSlice = useSelector(SelectPreferenceSlice);
+//     const [theme, setTheme] = useState(ConvertSystemTheme(PreferenceSlice.handlers.theme));
 
-    useEffect(() => {
-        themeMedia.addEventListener("change", _e => (theme == Theme.System) && UpdateTheme());
-    }, []);
+//     useEffect(() => {
+//         themeMedia.addEventListener("change", _e => (theme == Theme.System) && UpdateTheme());
+//     }, []);
 
-    useEffect(() => {
-        UpdateTheme();
-    }, [PreferenceSlice.handlers.theme]);
+//     useEffect(() => {
+//         UpdateTheme();
+//     }, [PreferenceSlice.handlers.theme]);
 
-    useEffect(() => {
-        const cssProperties: React.CSSProperties = props.themes.get(theme);
+//     useEffect(() => {
+//         const cssProperties: React.CSSProperties = props.themes.get(theme);
 
-        props.root.dataset["theme"] = theme;
+//         props.root.dataset["theme"] = theme;
 
-        if (cssProperties == null) { return; }
+//         if (cssProperties == null) { return; }
 
-        Object.entries(cssProperties).forEach(([key, value]) =>
-            props.root.style.setProperty(key, value));
+//         Object.entries(cssProperties).forEach(([key, value]) =>
+//             props.root.style.setProperty(key, value));
 
-        return () => {
-            Object.entries(cssProperties).forEach(([key, _value]) =>
-                props.root.style.setProperty(key, null));
-        };
-    }, [theme]);
+//         return () => {
+//             Object.entries(cssProperties).forEach(([key, _value]) =>
+//                 props.root.style.setProperty(key, null));
+//         };
+//     }, [theme]);
 
-    function UpdateTheme(): void {
-        setTheme(ConvertSystemTheme(PreferenceSlice.handlers.theme));
-    }
+//     function UpdateTheme(): void {
+//         setTheme(ConvertSystemTheme(PreferenceSlice.handlers.theme));
+//     }
 
-    return null;
-}
+//     return null;
+// }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import svgr from "vite-plugin-svgr";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({
-    include: "**/*.svg",
-  })],
-})
+  base: "/Wellness-Vet-Clinic/",
+  plugins: [react()],
+});
