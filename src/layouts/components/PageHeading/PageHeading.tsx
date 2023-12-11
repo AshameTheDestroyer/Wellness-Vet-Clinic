ec96044ddd87fccd5eef14512ed5687fb4071389
AshameTheDestroyer <hashemwnoos@gmail.com>
2023-12-11 11:07:08 +0200

    About page has been prototyped.

---
+++ src/layouts/components/PageHeading/PageHeading.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import ComponentProps from "../../../utils/types/ComponentProps";
+
+import "./PageHeading.scss";
+
+type PageHeadingProps = {
+    title: string;
+} & ComponentProps;
+
+export default function PageHeading(props: PageHeadingProps): React.ReactElement {
+    return (
+        <header
+            id={props.id}
+            className={[
+                "page-heading",
+                props.className,
+            ].toClassName()}
+        >
+            <h1>{props.title}</h1>
+            <section>{props.children}</section>
+        </header>
+    );
+}
--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 
 import "./AboutPage.scss";
+import PageHeading from "../../layouts/components/PageHeading/PageHeading";
 
 export default function AboutPage(): React.ReactElement {
     return (
         <main id="about-page">
-
+            <PageHeading title="About us" />
+            <main>
+                <figure></figure>
+                <main>
+                    <h2>About <strong>Wellness Veterinary Clinic</strong></h2>
+                    <p>
+                        Wellness Veterinary Clinic is an animal pet healthcare provider that was founded in October 2013 by a team of dedicated animal lovers and supported by the passionate animal care team. We have advanced throughout the years to deliver other products and services as well to cater to almost all the requirements and needs of our clients and their beloved pets.
+                    </p>
+                    <p>
+                        At Canadian Veterinary Clinic we offer veterinary services all week long. Our working hours at our branch in Al Manara, Dubai is from 9am-9pm all days except Saturday (Timing 1pm-9pm). At our branch in Al Zahiyah, Abu Dhabi the timing is from 8am-10pm except Saturday (Timing 2pm-10pm). We also provide emergency services and have a dedicated team working 24/7 to ensure the safety of pets.
+                    </p>
+                </main>
+            </main>
+            <section id="service-displayer">
+                <h2>Our Services</h2>
+                <main> {
+                    new Array(4)
+                        .fill(null)
+                        .map((_value, i) =>
+                            <div className="service-card" key={i}>Service {i + 1}</div>
+                        )
+                } </main>
+            </section>
         </main>
     );
 }
