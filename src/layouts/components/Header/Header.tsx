9853bd4468877f0011dd0b329ab9cac9986e45ef
AshameTheDestroyer <hashemwnoos@gmail.com>
2023-12-11 09:12:49 +0200

    Some adjustment have been just added.

--- src/layouts/components/Header/Header.tsx
+++ src/layouts/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import NavigationBar from "../../../components/NavigationBar/NavigationBar.tsx";
 
@@ -12,15 +12,41 @@ import REGEX_PATTERNS from "../../../utils/constants/RegexPatterns.tsx";
 import CrossIcon from "../../../assets/icons/CrossIcon.tsx";
 
 import parrot_image from "../../../assets/images/transparent/parrot.png";
+import HamburgerButton from "../../../utils/components/HamburgerButton/HamburgerButton.tsx";
+import HamburgerButtonIcon from "../../../assets/icons/HamburgerButtonIcon.tsx";
 
 export default function Header(): React.ReactElement {
     const [isSigningModalOpen, setIsSigningModalOpen] = useState<boolean>(false);
+    const [isNavigationContainerOpen, setIsNavigationContainerOpen] = useState<boolean>(false);
 
     return (
         <header id="header" className="sticking-header">
             <Logo />
-            <NavigationBar anchors={PAGE_ANCHORS} />
-            <button onClick={_e => setIsSigningModalOpen(true)}>Sign in</button>
+
+            <button id="hamburger-button" onClick={_e => setIsNavigationContainerOpen(true)}>
+                <HamburgerButtonIcon />
+            </button>
+            <section
+                id="navigation-background"
+
+                data-is-visible={isNavigationContainerOpen}
+
+                onClick={_e => setIsNavigationContainerOpen(false)}
+            />
+            <section
+                id="navigation-container"
+
+                data-is-open={isNavigationContainerOpen}
+            >
+                <NavigationBar anchors={PAGE_ANCHORS} />
+                <button
+                    id="sign-in-button"
+
+                    onClick={_e => setIsSigningModalOpen(true)}
+                >
+                    Sign in
+                </button>
+            </section>
 
             <SigningModal
                 isOpen={isSigningModalOpen}
@@ -44,6 +70,10 @@ type SigningModalProps = {
 function SigningModal(props: SigningModalProps): React.ReactElement {
     const [signingMethod, setSigningMethod] = useState<SigningMethod>(SigningMethod.SignIn);
 
+    useEffect(() => {
+        setSigningMethod(SigningMethod.SignIn);
+    }, [props.isOpen]);
+
     return (
         <Modal
             id="signing-modal"
@@ -57,7 +87,7 @@ function SigningModal(props: SigningModalProps): React.ReactElement {
 
             setIsOpen={props.setIsOpen}
         >
-            <button id="signing-modal-button" onClick={_e => props.setIsOpen(false)}>
+            <button id="signing-modal-close-button" onClick={_e => props.setIsOpen(false)}>
                 <CrossIcon />
             </button>
 
