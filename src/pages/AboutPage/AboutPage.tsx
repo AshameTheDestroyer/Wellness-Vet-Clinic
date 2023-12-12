ec2b193439ade86fa3213b5196050d87e8458f9b
AshameTheDestroyer <hashemwnoos@gmail.com>
2023-12-12 15:20:33 +0200

    About page adjustments.

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -24,7 +24,7 @@ export default function AboutPage(): React.ReactElement {
                         Wellness Veterinary Clinic is an animal pet healthcare provider that was founded in October 2013 by a team of dedicated animal lovers and supported by the passionate animal care team. We have advanced throughout the years to deliver other products and services as well to cater to almost all the requirements and needs of our clients and their beloved pets.
                     </p>
                     <p>
-                        At Canadian Veterinary Clinic we offer veterinary services all week long. Our working hours at our branch in Al Manara, Dubai is from 9am-9pm all days except Saturday (Timing 1pm-9pm). At our branch in Al Zahiyah, Abu Dhabi the timing is from 8am-10pm except Saturday (Timing 2pm-10pm). We also provide emergency services and have a dedicated team working 24/7 to ensure the safety of pets.
+                        At Wellness Veterinary Clinic we offer veterinary services all week long. Our working hours at our branch in Al Manara, Dubai is from 9am-9pm all days except Saturday (Timing 1pm-9pm). At our branch in Al Zahiyah, Abu Dhabi the timing is from 8am-10pm except Saturday (Timing 2pm-10pm). We also provide emergency services and have a dedicated team working 24/7 to ensure the safety of pets.
                     </p>
                 </main>
             </main>
